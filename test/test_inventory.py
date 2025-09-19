671e353ea7f33a86c7445fdfd240005b6979a4d3
Jerry Dhamyan Finol Sanchez <Jerrytareas17@gmail.com>
2025-09-19 17:46:15 -0400

    Add CI/CD with GitHub Actions and requirements.txt

--- test/test_inventory.py
+++ test/test_inventory.py
@@ -12,6 +12,7 @@ from config import config_for_login_page
 def inventory_page(request):
     firefox_options = FirefoxOptions()
     firefox_options.add_argument("--disable-notifications")
+    firefox_options.add_argument("--headless")
     firefox_options.set_preference("security.password_lifetime", 0)
     firefox_options.set_preference("signon.rememberSignons", False)
     firefox_options.set_preference("signon.autofillForms", False)
